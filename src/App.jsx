35712a069dc9655740bfbc874622b85e78798260
Tade Maddonni <tadeomaddonni@gmail.com>
2022-10-19 18:42:27 -0300

    adding responsive

--- src/App.jsx
+++ src/App.jsx
@@ -3,23 +3,13 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Footer from "./components/common/footer/Footer";
 import Topbar from "./components/common/topbar/Topbar";
 import Home from "./pages/home/Home";
-import menuData from "./utils/data/data";
 import Contact from "./pages/contact/Contact";
 import Menu from "./pages/menu/Menu";
 import Cart from "./pages/cart/Cart";
 import CartContextProvider from "./context/cartContext/CartContext";
 import ItemDetail from "./pages/itemDetail/ItemDetail";
+
 function App() {
-    const elements = menuData.map((item) => {
-        return (
-            <div key={item.id}>
-                <img src={item.img} alt="" />
-                <h3>{item.name}</h3>
-                <p>{item.info}</p>
-                <span>${item.price}</span>
-            </div>
-        );
-    });
     return (
         <CartContextProvider>
             <BrowserRouter>
@@ -30,7 +20,7 @@ function App() {
                     <Route path="/contact" element={<Contact />} />
                     <Route path="/menu" element={<Menu />} />
                     <Route path="/cart" element={<Cart />} />
-                    <Route path="/item/:id" element={<ItemDetail />}/>
+                    <Route path="/item/:id" element={<ItemDetail />} />
                 </Routes>
                 <Footer />
             </BrowserRouter>
--- src/components/common/topbar/Topbar.jsx
+++ src/components/common/topbar/Topbar.jsx
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import CartWidget from "./cartWidget/CartWidget";
 import "./topbar.scss";
 
 const Topbar = () => {
+    const [menuOn, setMenuOn] = useState(false);
+    console.log(menuOn);
     return (
         <div className="topbarContainer">
             <nav className="topbar">
@@ -24,6 +26,60 @@ const Topbar = () => {
                     <CartWidget />
                     <button>Sign Up</button>
                 </div>
+            </nav>
+
+            <nav
+                className={
+                    menuOn ? "responsiveTopbar active" : "responsiveTopbar"
+                }
+            >
+                <div
+                    className="hamburger"
+                    onClick={() => {
+                        setMenuOn((prevState) => !prevState);
+                    }}
+                >
+                    <span className="line1"></span>
+                    <span className="line2"></span>
+                    <span className="line2"></span>
+                </div>
+                <nav
+                    className={
+                        menuOn
+                            ? "topbar responsive active"
+                            : "topbar responsive"
+                    }
+                >
+                    <div className="left">
+                        <ul>
+                            <li
+                                onClick={() => {
+                                    setMenuOn((prevState) => !prevState);
+                                }}
+                            >
+                                <Link to="/">Home</Link>
+                            </li>
+                            <li
+                                onClick={() => {
+                                    setMenuOn((prevState) => !prevState);
+                                }}
+                            >
+                                <Link to="/menu">Menu</Link>
+                            </li>
+                            <li
+                                onClick={() => {
+                                    setMenuOn((prevState) => !prevState);
+                                }}
+                            >
+                                <Link to="/contact">Contact</Link>
+                            </li>
+                        </ul>
+                    </div>
+                    <div className="right">
+                        <CartWidget />
+                        <button>Sign Up</button>
+                    </div>
+                </nav>
             </nav>
         </div>
     );
